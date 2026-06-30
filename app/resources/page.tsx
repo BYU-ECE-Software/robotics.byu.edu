af3e57083b8db7adbf256827429b73d522e41798
Roman Vish <romanv7@byu.edu>
2026-06-30 16:10:21 -0600

    Add resources page so it can build

---
+++ app/resources/page.tsx
@@ -0,0 +1,15 @@
+import PageHero from "@/components/PageHero";
+
+export default function ResourcesPage() {
+  return (
+    <>
+      <PageHero
+        title="Resources"
+        subtitle="Learn about the professors involved in robotics research at BYU"
+        image="/images/heroes/speakers-hero.webp"
+        heroText="Robotics at BYU"
+        heroSubtext=""
+      />
+    </>
+  );
+}
